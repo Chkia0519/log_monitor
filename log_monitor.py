b5350947a2fabe316ed4b985af345e1f2187b122
Chkia0519 <a0986161178@gmail.com>
2026-08-22 23:25:04 +0800

    優化監測器

---
+++ log_monitor.py
@@ -0,0 +1,48 @@
+import time
+
+
+LOG_FILE = "system.log"
+CHECK_INTERVAL = 3
+
+def read_logs():
+    with open(LOG_FILE, "r", encoding="utf-8") as file:
+        return file.readlines()
+
+logs = read_logs()
+
+processed_count = len(logs)
+
+print("開始監控 system.log...")
+try:
+    while True:
+
+        try:
+            current_logs = read_logs()
+            
+        except FileNotFoundError:
+            print("找不到 system.log，等待 Log 檔案恢復...")
+            time.sleep(3)
+            continue
+        
+        if len(current_logs) < processed_count:
+            processed_count = 0
+    
+        if len(current_logs) > processed_count:
+            new_logs = current_logs[processed_count:]
+    
+            for log in new_logs:
+                if "CRITICAL" in log:
+                    print(f"[嚴重異常] {log.strip()}")
+    
+                elif "ERROR" in log:
+                    print(f"[異常] {log.strip()}")
+    
+                elif "WARNING" in log:
+                    print(f"[警告] {log.strip()}")
+    
+            processed_count = len(current_logs)
+    
+        time.sleep(CHECK_INTERVAL)
+    
+except KeyboardInterrupt:
+    print("\n監控已停止")
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import logging
 
+
 logging.basicConfig(
     filename="system.log",
     level=logging.INFO,
@@ -7,10 +8,11 @@ logging.basicConfig(
     encoding="utf-8"
 )
 
-logging.info("系統啟動")
 logging.info("使用者登入成功")
 logging.warning("API 回應速度過慢")
 logging.error("資料庫連線失敗")
-logging.critical("系統無法打開")
+logging.critical("核心服務停止")
 
 print("Log 建立完成")
+
+logging.shutdown() #把 logging 使用中的檔案處理器關閉，釋放 Log 檔案。
