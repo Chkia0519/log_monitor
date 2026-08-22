d0b33db75c88e570f10c40e7e91524a03f6992ab
Chkia0519 <a0986161178@gmail.com>
2026-08-22 10:03:09 +0800

    加入顯示幾個Error數量及列出錯誤訊息

--- log_analyzer.py
+++ log_analyzer.py
@@ -13,8 +13,19 @@ for log in logs:
         warning_count += 1
 
     elif "ERROR" in log:
-        error_count += 1
+        error_count += 1    
 
 print("INFO 數量：", info_count)
 print("WARNING 數量：", warning_count)
 print("ERROR 數量：", error_count)
+
+if error_count > 0:
+    print(f"\n系統存在 {error_count} 筆異常")
+    print("ERROR 紀錄：")
+
+    for log in logs:
+        if "ERROR" in log:
+            print(log.strip())
+            
+else:
+    print("系統目前無 ERROR")
